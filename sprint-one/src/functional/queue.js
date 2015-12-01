de3607a79e4b981e39b29cd74304b386ac11c0d9
dkim1116 <daniel.kim1116@gmail.com>
2015-12-01 11:33:16 -0800

    finishes functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,38 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var storageArr = [];
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storageArr.push(value);
+    size = storageArr.length;
+    // console.log(storageArr)
+    for(var i=0; i<storageArr.length; i++){
+    storage[i] = storageArr[i];
+  }
+    return storage;
   };
 
   someInstance.dequeue = function(){
+  //   for(var i=0; i<storageArr.length; i++){
+  //   storage[i] = storageArr[i];
+  // }
+    storageArr.shift();
+    size = storageArr.length;
+    return storage[0];
   };
 
   someInstance.size = function(){
+    return size;
   };
-
+  console.log(storageArr)
+  // for(var i=0; i<storageArr.length; i++){
+  //   storage[i] = storageArr[i];
+  // }
+  // _.each(storageArr, function(val, index, list){
+    // storage[index] = storageArr[index];
+  // })
   return someInstance;
 };
